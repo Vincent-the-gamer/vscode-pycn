a24016a026e2f67743d32e42a9e919e8ffe9bbd1
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-07-31 11:17:56 +0800

    fix: release config.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { defineExtension } from 'reactive-vscode'
+import { defineExtension, onActivate } from 'reactive-vscode'
+import { window } from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  
+  onActivate(() => {
+    window.showInformationMessage("Pycn activated successfully!")
+  })
 })
 
 export { activate, deactivate }
